c9566ec7fcbc13cb93c5b1fca8ca5ed43cda80e6
isarnassiri <isar.nassiri@gmail.com>
2022-07-03 11:29:59 +0100

    Revised.

---
+++ RUN_ME.R
@@ -0,0 +1,18 @@
+#########################################################################
+# Please execute the code in the RStudio IDE (https://www.rstudio.com/) #
+#########################################################################
+
+# Install and load R package
+new.packages <- list.of.packages[!("rstudioapi" %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages, repos = "http://cran.us.r-project.org")
+library("rstudioapi") 
+setwd(paste0(dirname(rstudioapi::getActiveDocumentContext()$path), '/Scripts/')); 
+
+source("Generate_Interactive_QC_Report.R")
+
+# Generate an "Interactive QC Report"
+Generate_Interactive_QC_Report()
+
+############################################################ 
+# 'Find the "Interactive QC Report" in the Outputs/ folder #
+############################################################
