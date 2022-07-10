9924c996555de49d0ebab765259d6157f5fee7f2
“isarnassiri” <isar.nassiri@gmail.com>
2022-07-10 09:00:15 +0100

    Revised.

--- RUN_ME.R
+++ RUN_ME.R
@@ -3,10 +3,9 @@
 #########################################################################
 
 # Install and load R package
-list.of.packages <- c("rstudioapi")
 new.packages <- list.of.packages[!("rstudioapi" %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, repos = "http://cran.us.r-project.org")
-library("rstudioapi") 
+library("rstudioapi")
 setwd(paste0(dirname(rstudioapi::getActiveDocumentContext()$path), '/Scripts/')); 
 
 source("Generate_Interactive_QC_Report.R")
