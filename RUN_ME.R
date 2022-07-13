1899c9bb59565b7340672c02d284d115401d8c61
“isarnassiri” <isar.nassiri@gmail.com>
2022-07-13 15:54:46 +0100

    R.

--- RUN_ME.R
+++ RUN_ME.R
@@ -11,9 +11,9 @@ source("GenerateInteractiveQCReport.R")
 GenerateInteractiveQCReport()
 
 ##### Cell Type Enrichment Analysis #####
-# setwd("~/"); setwd(paste0(dirname(rstudioapi::getActiveDocumentContext()$path), '/Scripts/')); 
-# source("CellTypeEnrichment.R")
-# CellTypeEnrichment()
+setwd("~/"); setwd(paste0(dirname(rstudioapi::getActiveDocumentContext()$path), '/Scripts/'));
+source("CellTypeEnrichment.R")
+CellTypeEnrichment()
 
 ############################################################ 
 #  Find the "Interactive QC Report" in the Outputs/ folder #
