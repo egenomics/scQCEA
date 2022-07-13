3817d15a5a48bfeafd00b505630c9fddbdbf484a
“isarnassiri” <isar.nassiri@gmail.com>
2022-07-13 14:25:54 +0100

    R.

--- RUN_ME.R
+++ RUN_ME.R
@@ -5,15 +5,15 @@
 ##### Install and load R packages #####
 if(!("rstudioapi" %in% installed.packages()[,"Package"])) install.packages("rstudioapi", repos = "http://cran.us.r-project.org"); library("rstudioapi")
 
-##### Cell Type Enrichment Analysis #####
-# setwd("~/"); setwd(paste0(dirname(rstudioapi::getActiveDocumentContext()$path), '/Scripts/')); 
-# source("CellTypeEnrichment.R")
-# CellTypeEnrichment()
-
 ##### Generate an "Interactive QC Report" #####
 setwd("~/"); setwd(paste0(dirname(rstudioapi::getActiveDocumentContext()$path), '/Scripts/')); 
 source("GenerateInteractiveQCReport.R")
 GenerateInteractiveQCReport()
+
+##### Cell Type Enrichment Analysis #####
+# setwd("~/"); setwd(paste0(dirname(rstudioapi::getActiveDocumentContext()$path), '/Scripts/')); 
+# source("CellTypeEnrichment.R")
+# CellTypeEnrichment()
 
 ############################################################ 
 #  Find the "Interactive QC Report" in the Outputs/ folder #
